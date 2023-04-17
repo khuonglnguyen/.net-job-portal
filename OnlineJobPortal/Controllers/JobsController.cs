ce4667ac42365d388ed7e02894cef18bd65b9c75
victornguyen0412 <victor.nguyen@beyondedge.co>
2023-04-17 15:49:17 +0700

    Add project files.

---
+++ OnlineJobPortal/Controllers/JobsController.cs
@@ -0,0 +1,50 @@
+﻿using OnlineJobPortal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineJobPortal.Controllers
+{
+    public class JobsController : Controller
+    {
+        JobPortalDBEntities _context = new JobPortalDBEntities();
+        // GET: Job
+        public ActionResult Index()
+        {
+            var listJobs = _context.Jobs.ToList();
+            return View(listJobs);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var job = _context.Jobs.Find(id);
+            ViewBag.message = TempData["message"] != null ? TempData["message"].ToString() : null;
+
+            var user = Session["User"] as User != null ? Session["User"] as User : null;
+            var appliedCheck = user != null ? _context.AppliedJobs.ToList().LastOrDefault(x => x.JobId == id && x.UserId == user.UserId ) : null;
+            ViewBag.appliedCheck = appliedCheck;
+            return View(job);
+        }
+
+        public ActionResult Apply(int id)
+        {
+            var user = Session["User"] as User;
+            if (user == null)
+            {
+                return Redirect("/User/Login");
+            }
+
+            var applyJob = new AppliedJob();
+            applyJob.JobId = id;
+            applyJob.UserId = (Session["User"] as User).UserId;
+
+            _context.AppliedJobs.Add(applyJob);
+            _context.SaveChanges();
+
+            TempData["message"] = "Job applied successfull!";
+            return RedirectToAction("Detail", new { id = id });
+        }
+    }
+}
---
+++ OnlineJobPortal/Controllers/UserController.cs
@@ -0,0 +1,62 @@
+﻿using OnlineJobPortal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineJobPortal.Controllers
+{
+    public class UserController : Controller
+    {
+        JobPortalDBEntities _context = new JobPortalDBEntities();
+        // GET: User
+        public ActionResult Register()
+        {
+            var listCountry = _context.Countries.ToList();
+            ViewBag.listCountry = listCountry;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(User user)
+        {
+            var result = _context.Users.Add(user);
+            _context.SaveChanges();
+            if (result != null)
+            {
+                ViewBag.message = "Register Successfully!";
+            }
+            else
+            {
+                var listCountry = _context.Countries.ToList();
+                ViewBag.listCountry = listCountry;
+            }
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            var check = _context.Users.SingleOrDefault(x => x.Username == user.Username && x.Password == user.Password);
+            if (check != null)
+            {
+                Session["User"] = check;
+                return Redirect("/");
+            }
+            ViewBag.message = "Username or Password is incorrect!";
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session["User"] = null;
+            return Redirect("/User/Login");
+        }
+    }
+}
