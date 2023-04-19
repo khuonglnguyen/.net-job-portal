bdbd47b91c535b11b319df6beb3430cf445c90e0
victornguyen0412 <victor.nguyen@beyondedge.co>
2023-04-19 15:13:53 +0700

    Add filter job type

--- OnlineJobPortal/Controllers/JobsController.cs
+++ OnlineJobPortal/Controllers/JobsController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Services.Description;
 
 namespace OnlineJobPortal.Controllers
 {
@@ -17,13 +18,28 @@ namespace OnlineJobPortal.Controllers
             return View(listJobs);
         }
 
+        [HttpPost]
+        public ActionResult Index(string[] types)
+        {
+            if (types != null)
+            {
+                var listJobs = _context.Jobs.Where(x => types.Contains(x.JobType)).ToList();
+                return PartialView("JobPartial", listJobs);
+            }
+            else
+            {
+                var listJobs = _context.Jobs.ToList();
+                return PartialView("JobPartial", listJobs);
+            }
+        }
+
         public ActionResult Detail(int id)
         {
             var job = _context.Jobs.Find(id);
             ViewBag.message = TempData["message"] != null ? TempData["message"].ToString() : null;
 
             var user = Session["User"] as User != null ? Session["User"] as User : null;
-            var appliedCheck = user != null ? _context.AppliedJobs.ToList().LastOrDefault(x => x.JobId == id && x.UserId == user.UserId ) : null;
+            var appliedCheck = user != null ? _context.AppliedJobs.ToList().LastOrDefault(x => x.JobId == id && x.UserId == user.UserId) : null;
             ViewBag.appliedCheck = appliedCheck;
             return View(job);
         }
