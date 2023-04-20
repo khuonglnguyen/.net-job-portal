bea84c6aacabec9ad1963a336babfb1588733053
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2023-04-20 14:23:47 +0700

    Add Edit Job Admin

--- OnlineJobPortal/Controllers/AdminController.cs
+++ OnlineJobPortal/Controllers/AdminController.cs
@@ -23,11 +23,8 @@ namespace OnlineJobPortal.Controllers
 
         public ActionResult NewJobs()
         {
-            ViewBag.TotalUsers = _context.Users.Count();
-            ViewBag.TotalJobs = _context.Jobs.Count();
-            ViewBag.AppliedJobs = _context.AppliedJobs.Count();
-            ViewBag.ContactedUsers = _context.Contacts.Count();
-            return View();
+            var countries = _context.Countries.OrderBy(x => x.CountryName).ToList();
+            return View(countries);
         }
 
         [HttpPost]
@@ -57,6 +54,55 @@ namespace OnlineJobPortal.Controllers
         {
             var list = _context.Jobs.ToList();
             return View(list);
+        }
+
+
+        public ActionResult JobsEdit(int id)
+        {
+            var job = _context.Jobs.Find(id);
+            ViewBag.countries = _context.Countries.OrderBy(x => x.CountryName).ToList();
+            return View(job);
+        }
+
+        [HttpPost]
+        public ActionResult JobsEdit(Job job, HttpPostedFileBase companyImage)
+        {
+            var jobUpdate = _context.Jobs.Find(job.JobId);
+            if (companyImage != null)
+            {
+                //Get file name
+                var fileName = Path.GetFileName(companyImage.FileName);
+                //Get path
+                var path = Path.Combine(Server.MapPath("~/assets/img/icon"), fileName);
+                //Check exitst
+                if (!System.IO.File.Exists(path))
+                {
+                    //Add image into folder
+                    companyImage.SaveAs(path);
+                }
+
+                jobUpdate.CompanyImage = fileName;
+            }
+
+            jobUpdate.Title = job.Title;
+            jobUpdate.NoOfPost = job.NoOfPost;
+            jobUpdate.Description = job.Description;
+            jobUpdate.Qualification = job.Qualification;
+            jobUpdate.Experience = job.Experience;
+            jobUpdate.Specialization = job.Specialization;
+            jobUpdate.LastDateToApply = job.LastDateToApply;
+            jobUpdate.Salary = job.Salary;
+            jobUpdate.JobType = job.JobType;
+            jobUpdate.CompanyName = job.CompanyName;
+            jobUpdate.Website = job.Website;
+            jobUpdate.Email = job.Email;
+            jobUpdate.Address = job.Address;
+            jobUpdate.Country = job.Country;
+            jobUpdate.State = job.State;
+
+            _context.SaveChanges();
+
+            return Redirect("/Admin/Jobs");
         }
     }
 }
