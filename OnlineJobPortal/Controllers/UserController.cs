0d933543831b92fff8595933f6ae829075a24029
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2023-04-25 21:44:03 +0700

    Add edit resume

--- OnlineJobPortal/Controllers/UserController.cs
+++ OnlineJobPortal/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using OnlineJobPortal.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -34,7 +35,7 @@ namespace OnlineJobPortal.Controllers
             }
             else
             {
-                var listCountry = _context.Countries.ToList();
+                var listCountry = _context.Countries.OrderBy(x => x.CountryName).ToList();
                 ViewBag.listCountry = listCountry;
             }
             return View();
@@ -66,6 +67,60 @@ namespace OnlineJobPortal.Controllers
         {
             Session["User"] = null;
             return Redirect("/User/Login");
+        }
+
+        public ActionResult EditResume()
+        {
+            var listCountry = _context.Countries.OrderBy(x => x.CountryName).ToList();
+            ViewBag.listCountry = listCountry;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EditResume(User user, HttpPostedFileBase resume)
+        {
+            string fileName = "";
+            if (resume != null)
+            {
+                //Get file name
+                fileName = Path.GetFileName(resume.FileName);
+                //Get path
+                var path = Path.Combine(Server.MapPath("~/Resume"), fileName);
+                //Check exitst
+                if (!System.IO.File.Exists(path))
+                {
+                    //Add image into folder
+                    resume.SaveAs(path);
+                }
+            }
+
+            var userUpdate = _context.Users.Find(user.UserId);
+            userUpdate.Username = user.Username;
+            userUpdate.Name = user.Name;
+            userUpdate.Email = user.Email;
+            userUpdate.Mobile = user.Mobile;
+            userUpdate.TenthGrade = user.TenthGrade;
+            userUpdate.TwelfthGrade = user.TwelfthGrade;
+            userUpdate.GraduationGrade = user.GraduationGrade;
+            userUpdate.PostGraduationGrade = user.PostGraduationGrade;
+            userUpdate.Phd = user.Phd;
+            userUpdate.WorksOn = user.WorksOn;
+            userUpdate.Experience = user.Experience;
+            if (fileName != null)
+            {
+                userUpdate.Resume = fileName;
+            }
+            userUpdate.Address = user.Address;
+            userUpdate.Country = user.Country;
+
+            _context.SaveChanges();
+
+            var listCountry = _context.Countries.OrderBy(x => x.CountryName).ToList();
+            ViewBag.listCountry = listCountry;
+            ViewBag.message = "Success";
+
+            Session["User"] = userUpdate;
+            return View();
         }
     }
 }
